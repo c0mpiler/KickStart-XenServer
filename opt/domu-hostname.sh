db8049558f218f41aa67d9baae23abd67bc7951d
c0mpiler <stack0verflow@outlook.com>
2014-07-29 21:16:15 -0700

    Create domu-hostname.sh

---
+++ opt/domu-hostname.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Detect if xenstore-read is available
+XENSTOREREAD=`which xenstore-read`
+if [ -e $XENSTOREREAD ]; then
+	# Filter the domU's name to a lowercase alphanumeric hyphenated hostname
+	NAME=`xenstore-read name | sed -e 's/[^[:alnum:]|-]/-/g' | tr '[:upper:]' '[:lower:]'`
+
+	# Don't do anything if this name is blank
+	[ "$NAME" = "" ] && exit 0
+
+	# Set the hostname
+	echo "$NAME" > /etc/hostname
+	/bin/hostname -F /etc/hostname
+fi
+exit 0
