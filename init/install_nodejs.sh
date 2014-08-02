4741fa05175d3df0918b68bfffa7ba6840929b86
c0mpiler <stack0verflow@outlook.com>
2014-08-01 21:55:13 -0700

    Create install_nodejs.sh

---
+++ init/install_nodejs.sh
@@ -0,0 +1,9 @@
+# Installing NodeJS and NPM
+
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get update -y
+sudo apt-get install -y nodejs
+sudo curl https://www.npmjs.org/install.sh | sudo sh
+node -v
+npm -v
